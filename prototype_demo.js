7e802e79357cdd5a2115586a43a191405baae0c7
Raja Jaganathan <rajajflex@gmail.com>
2015-08-22 23:52:52 +0530

    IFEE added and commented

--- prototype_demo.js
+++ prototype_demo.js
@@ -1,97 +1,110 @@
-function Point(x,y){
-	this.x = x;
-	this.y = y;	
-}
+/**
+	@author Raja Jaganathan
+*/
 
-function Shape(elem, points){
-	this.lines = [];
-	this.points = points || [];
-	this.elem = elem;
-	this.init();
-}
+(function() {
 
-Shape.prototype = {
-	constructor:Shape,
-	init:function(){
-		this.context = this.context || document.getElementById(this.elem).getContext('2d');		
-	},
-	draw:function(){
-		var ctx = this.context;
-		ctx.fillStyle = 'green';
-		ctx.beginPath();
+    'use strict';
 
-		ctx.moveTo(this.points[0].x,this.points[0].y);
-		for (var idx = 1; idx < this.points.length; idx++) {			
-			ctx.lineTo(this.points[idx].x,this.points[idx].y);ctx.fill();
-		}
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
+    }
 
-		ctx.stroke();
-		ctx.closePath();
-	}
-};
+    function Shape(elem, points) {
+        this.lines = [];
+        this.points = points || [];
+        this.elem = elem;
+        this.init();
+    }
 
-function extend(Child, Parent){
-	Child.prototype = Object.create(Parent.prototype);
-	Child.prototype.constructor = Child;
-}
+    Shape.prototype = {
+        constructor: Shape,
+        init: function() {
+            this.context = this.context || document.getElementById(this.elem).getContext('2d');
+        },
+        draw: function() {
+            var ctx = this.context;
+            ctx.fillStyle = 'green';
+            ctx.beginPath();
 
-function Square(elem, points){
-	Shape.apply(this, arguments);	
-}
+            ctx.moveTo(this.points[0].x, this.points[0].y);
+            for (var idx = 1; idx < this.points.length; idx++) {
+                ctx.lineTo(this.points[idx].x, this.points[idx].y);
+                ctx.fill();
+            }
 
-extend(Square,Shape);
-// Square.prototype = Object.create(Shape.prototype);
-// Square.prototype.constructor = Square;
+            ctx.stroke();
+            ctx.closePath();
+        }
+    };
+
+    function extend(Child, Parent) {
+        Child.prototype = Object.create(Parent.prototype);
+        Child.prototype.constructor = Child;
+    }
+
+    function Square(elem, points) {
+        Shape.apply(this, arguments);
+    }
+
+    extend(Square, Shape);
+    // Square.prototype = Object.create(Shape.prototype);
+    // Square.prototype.constructor = Square;
 
 
-function Rectangle(elem, points){
-	Square.apply(this, arguments);	//Note here Square instead of Shape
-}
+    function Rectangle(elem, points) {
+        Square.apply(this, arguments); //Note here Square instead of Shape
+    }
 
-function Triangle(elem, points){
-	Shape.apply(this, arguments);
-}
+    function Triangle(elem, points) {
+        Shape.apply(this, arguments);
+    }
 
-extend(Triangle,Shape);
-// Triangle.prototype = Object.create(Shape.prototype);
-// Triangle.prototype.constructor = Triangle;
+    extend(Triangle, Shape);
+    // Triangle.prototype = Object.create(Shape.prototype);
+    // Triangle.prototype.constructor = Triangle;
 
-function Circle(elem, points){
-	Shape.apply(this, arguments);	
-	this.points = points;
-}
+    function Circle(elem, points) {
+        Shape.apply(this, arguments);
+        this.points = points;
+    }
 
-extend(Circle,Shape);
-// Circle.prototype = Object.create(Shape.prototype);
-// Circle.prototype.constructor = Circle;
-Circle.prototype.draw = function(){
-	var ctx = this.context;	
-	ctx.beginPath();
-	ctx.fillStyle = 'red';
-	ctx.arc(this.points.centerX, this.points.centerY, this.points.radius, 0, Math.PI*2, true); 	
-	ctx.fill();
-};
+    extend(Circle, Shape);
+    // Circle.prototype = Object.create(Shape.prototype);
+    // Circle.prototype.constructor = Circle;
+    Circle.prototype.draw = function() {
+        var ctx = this.context;
+        ctx.beginPath();
+        ctx.fillStyle = 'red';
+        ctx.arc(this.points.centerX, this.points.centerY, this.points.radius, 0, Math.PI * 2, true);
+        ctx.fill();
+    };
 
-var p1  = new Point(100,100);
-var p2  = new Point(200,100);
-var p3  = new Point(200,200);
-var p4  = new Point(100,200);
+    //For Square
+    var p1 = new Point(100, 100);
+    var p2 = new Point(200, 100);
+    var p3 = new Point(200, 200);
+    var p4 = new Point(100, 200);
 
-var shape1 = new Shape('canvas',[p1,p2,p3,p4]);
-shape1.draw();
+    var squareInstance = new Square('canvas', [p1, p2, p3, p4]);
+    squareInstance.draw();
 
-var circle1 = new Circle('canvas1',{
-	centerX:150,
-	centerY:150,
-	radius:60
-});
+    //For Circle
+    var circleInstance = new Circle('canvas1', {
+        centerX: 150,
+        centerY: 150,
+        radius: 60
+    });
 
-circle1.draw();
+    circleInstance.draw();
 
-var p1  = new Point(100,100);
-var p2  = new Point(130,200);
-var p3  = new Point(200,100);
+    //For Triangle
+    p1 = new Point(100, 100);
+    p2 = new Point(130, 200);
+    p3 = new Point(200, 100);
 
-var triangle1 = new Triangle('canvas2',[p1,p2,p3]);
-triangle1.draw();
+    var triangleInstance = new Triangle('canvas2', [p1, p2, p3]);
+    triangleInstance.draw();
 
+}());
